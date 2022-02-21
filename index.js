7931c8690057f98799d9eebac8e12c8d6ff6dc22
Zouhairhajji <hajji.zouhair@outlook.fr>
2022-02-21 21:50:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,21 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
-client.config = config;
+const prefix = "!=";
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
 
-client.login(config.token);
+client.on('message', message => {
+  const shouldIgnoreMsg = !message.content.startsWith(prefix) || message.author.bot
+  if(shouldIgnoreMsg) return
+  const args = message.content.slice(prefix).split(/ */)
+  const command = args.shift().toLowerCase();
+
+
+  message.channel.send("nique ta mere")
+})
+
+
+client.login(process.env.TOKEN);
